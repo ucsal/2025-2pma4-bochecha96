841bd08e940e3c6d721d36308c2c65ca7780f87f
Alexandre Amorim <alexandrepinto2005@hotmail.com>
2025-09-10 00:01:33 -0300

    factorys implementadas

--- src/main/java/br/com/mariojp/figureeditor/DrawingPanel.java
+++ src/main/java/br/com/mariojp/figureeditor/DrawingPanel.java
@@ -11,32 +11,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 class DrawingPanel extends JPanel {
-
     private static final long serialVersionUID = 1L;
     private static final int DEFAULT_SIZE = 60;
     private final List<Shape> shapes = new ArrayList<>();
     private Point startDrag = null;
+    private ShapeFactory shapeFactory = new CircleFactory();
 
     DrawingPanel() {
-        
         setBackground(Color.WHITE);
         setOpaque(true);
         setDoubleBuffered(true);
 
         var mouse = new MouseAdapter() {
-            @Override public void mouseClicked(MouseEvent e) {
+            @Override
+            public void mouseClicked(MouseEvent e) {
                 if (e.getClickCount() == 1 && startDrag == null) {
                     int size = Math.max(Math.min(DEFAULT_SIZE, DEFAULT_SIZE), 10);
-                    Shape s =  new Ellipse2D.Double(e.getPoint().x, e.getPoint().y, size, size);
-                    //return new Rectangle2D.Double(e.getPoint().x, e.getPoint().y, Math.max(DEFAULT_SIZE, 10), Math.max(DEFAULT_SIZE, 10));
+                    Shape s = shapeFactory.createShape(e.getPoint(), size);
                     shapes.add(s);
                     repaint();
                 }
             }
         };
-        addMouseListener(mouse);        
+        addMouseListener(mouse);
         addMouseMotionListener(mouse);
-
     }
 
     void clear() {
@@ -44,7 +42,8 @@ class DrawingPanel extends JPanel {
         repaint();
     }
 
-    @Override protected void paintComponent(Graphics g) {
+    @Override
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g.create();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
@@ -59,5 +58,4 @@ class DrawingPanel extends JPanel {
 
         g2.dispose();
     }
-
 }
---
+++ src/main/java/br/com/mariojp/figureeditor/ShapeFactory.java
@@ -0,0 +1,6 @@
+package br.com.mariojp.figureeditor;
+import java.awt.Shape;
+import java.awt.Point;
+public interface ShapeFactory {
+    Shape createShape(Point p, int size);
+}
